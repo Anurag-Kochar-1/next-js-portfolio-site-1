6ec19a4d7ab0f85431358473ca50afcc69992a0d
Anurag <anuragmarketing101@gmail.com>
2022-10-27 22:54:36 +0530

    fixed-3

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import BgAnimation from '../components/BackgroundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
+// import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
@@ -18,7 +18,7 @@ const Home:React.FC = () => {
       </Section>
       <Projects />
       <Technologies />
-      <Timeline />
+      {/* <Timeline /> */}
       <Acomplishments />
     </Layout>
   );
