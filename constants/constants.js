7567562e39ed94a1f6f3659303c8359b140bef27
Anurag <anuragmarketing101@gmail.com>
2022-11-16 13:02:29 +0530

    constants-updated

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,14 @@
 export const projects = [
+
+    {
+      title: 'Medium Clone',
+      description:"A Blog app where user can upload, read, like, and bookmark their favourite blog. User can read through genres with the help of category selector header which is available for all types of screens along with the bookmark page where reader can view their bookmarked blogs ",
+      image: '/images/thumbnail-medium.png',
+      tags: ['Next JS', 'Sanity CMS' , 'Tailwind', "Typescript"],
+      source: 'https://github.com/Anurag-Kochar-1/medium-clone-nextjs-1',
+      visit: 'https://medium-clone-nextjs-1.vercel.app/',
+      id: 0,
+    },
     {
       title: 'Twitter Clone',
       description: "A clone in which user can sign in with google and post a tweet with his account where other users can comment below the tweet",
@@ -6,17 +16,17 @@ export const projects = [
         tags: ['Next JS', 'Sanity CMS' , 'Tailwind', "Typescript"],
       source: 'https://github.com/Anurag-Kochar-1/twitter-clone-nextjs-1',
       visit: 'https://twitter-clone-nextjs-1.netlify.app/',
-      id: 0,
+      id: 1,
     },
 
       {
       title: 'Litpire The Movie App',
-      description: " A movie app where user can view popular, top rated, upcoming movies as well tv shows with the functionality of choosing 15+ genres.",
+      description: "A movie app where user can view popular, top rated, upcoming movies as well tv shows with the functionality of choosing 15+ genres.",
         image: '/images/thumbnail-litpire.png',
         tags: ['React', 'Tailwind' , "Javacript"],
       source: 'https://github.com/Anurag-Kochar-1?tab=repositories',
       visit: 'https://flimpire-clone-1-v-6.netlify.app/',
-      id: 1,
+      id: 2,
     },
     {
       title: 'YouTube Clone',
@@ -25,18 +35,9 @@ export const projects = [
       tags: ['React', 'MUI' , "Javacript"],
       source: 'https://github.com/Anurag-Kochar-1/youtube-clone-1',
       visit: 'https://youtube-clone-site-1.netlify.app/',
-      id: 2,
-    },
-
-    {
-      title: 'Spotify Clone',
-      description:"A Music app where user can listen top trending songs (this project is not completed yet) ",
-      image: '/images/thumbnail-music.png',
-      tags: ['React', 'Tailwind' , "Javacript"],
-      source: 'https://github.com/Anurag-Kochar-1/music-app-clone-1',
-      visit: 'https://music-app-clone-react-1.netlify.app/',
       id: 3,
     }
+
     
   ];
   
